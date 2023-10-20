d7d8780b45fa3ce94a9896547cfaf53ae433911d
pEvgeniy <popevgeniyv@yandex.ru>
2023-10-20 15:16:52 +0300

    added compilation services

---
+++ main-service/src/main/java/ru/practicum/service/compilation/CompilationsPublicService.java
@@ -0,0 +1,13 @@
+package ru.practicum.service.compilation;
+
+import ru.practicum.dto.compilation.CompilationDto;
+
+import java.util.List;
+
+public interface CompilationsPublicService {
+
+    List<CompilationDto> findCompilations( Boolean pinned, Integer from, Integer size);
+
+    CompilationDto findCompilationById(Integer compId);
+
+}
