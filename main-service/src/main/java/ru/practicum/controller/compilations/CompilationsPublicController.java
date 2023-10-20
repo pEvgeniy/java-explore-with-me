ecc078701d5bb72681c112770cd42e81aa08b202
pEvgeniy <popevgeniyv@yandex.ru>
2023-10-20 15:12:47 +0300

    added compilations controllers

---
+++ main-service/src/main/java/ru/practicum/controller/compilations/CompilationsPublicController.java
@@ -0,0 +1,38 @@
+package ru.practicum.controller.compilations;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.dto.compilation.CompilationDto;
+import ru.practicum.service.compilation.CompilationsPublicService;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/compilations")
+@RequiredArgsConstructor
+public class CompilationsPublicController {
+
+    private final CompilationsPublicService compilationsService;
+
+    @GetMapping
+    public List<CompilationDto> findCompilations(@RequestParam Boolean pinned,
+                                                 @RequestParam(defaultValue = "0") Integer from,
+                                                 @RequestParam(defaultValue = "10") Integer size) {
+        log.info("controller. get. /compilations. find compilations with parameters = [pinned = {}, from = {}, size = {}]",
+                pinned, from, size);
+        return compilationsService.findCompilations(pinned, from, size);
+    }
+
+    @GetMapping("/{compId}")
+    public CompilationDto findCompilationById(@PathVariable Integer compId) {
+        log.info("controller. get. /compilations/{}. find compilation", compId);
+        return compilationsService.findCompilationById(compId);
+    }
+
+}
