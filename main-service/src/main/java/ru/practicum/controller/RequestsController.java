ee6e43ab4c47d6ded18872355d93f1b3eff5abae
pEvgeniy <popevgeniyv@yandex.ru>
2023-10-24 14:35:47 +0300

    validation fixes (request params and response statuses)

--- main-service/src/main/java/ru/practicum/controller/RequestsController.java
+++ main-service/src/main/java/ru/practicum/controller/RequestsController.java
@@ -2,12 +2,14 @@ package ru.practicum.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.dto.requests.ParticipationRequestDto;
 import ru.practicum.service.requests.RequestsService;
@@ -29,6 +31,7 @@ public class RequestsController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ParticipationRequestDto createRequest(@PathVariable Integer userId,
                                                  @RequestParam Integer eventId) {
         log.info("controller. post. /users/{}/requests/?eventId={}. trying to find requests for user", userId, eventId);
--- main-service/src/main/java/ru/practicum/controller/UserController.java
+++ main-service/src/main/java/ru/practicum/controller/UserController.java
@@ -2,6 +2,7 @@ package ru.practicum.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.dto.user.UserDto;
 import ru.practicum.service.user.UserService;
@@ -27,8 +29,8 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
-    public List<UserDto> findAllUsersWithParameters(@RequestParam List<Integer> ids,
-                                                    @RequestParam(defaultValue = "10") Integer from,
+    public List<UserDto> findAllUsersWithParameters(@RequestParam(required = false) List<Integer> ids,
+                                                    @RequestParam(defaultValue = "0") Integer from,
                                                     @RequestParam(defaultValue = "10") Integer size) {
         log.info("controller. get. /admin/users. find all suers with parameters = [ids = {}, from = {}, size = {}]",
                 ids, from, size);
@@ -36,12 +38,14 @@ public class UserController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public UserDto addUser(@Valid @RequestBody UserDto userDto) {
         log.info("controller. post. /admin/users. create user with body = {}", userDto);
         return userService.createUser(userDto);
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteUser(@PathVariable Integer id) {
         log.info("controller. post. /admin/users. delete user with id = {}", id);
         userService.deleteUserById(id);
