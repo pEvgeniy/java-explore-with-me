85320122b7f20b9119a7999fb008928fe277bccb
pEvgeniy <popevgeniyv@yandex.ru>
2023-10-24 14:39:42 +0300

    repository queries fix

--- main-service/src/main/java/ru/practicum/repository/CategoryRepository.java
+++ main-service/src/main/java/ru/practicum/repository/CategoryRepository.java
@@ -4,4 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.model.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+    Boolean existsByName(String name);
+
 }
--- main-service/src/main/java/ru/practicum/repository/EventRepository.java
+++ main-service/src/main/java/ru/practicum/repository/EventRepository.java
@@ -3,50 +3,65 @@ package ru.practicum.repository;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
-import ru.practicum.controller.events.EventsPublicController;
+import org.springframework.data.repository.query.Param;
 import ru.practicum.dto.events.EventFullDto;
+import ru.practicum.dto.events.EventFullDto.EventState;
 import ru.practicum.model.Event;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
-public interface EventRepository extends JpaRepository<Event, Integer> {
+public interface EventRepository extends JpaRepository<Event, Integer>, JpaSpecificationExecutor<Event> {
 
-    @Query("SELECT MIN(e.publishedAt) " +
+    @Query("SELECT MIN(e.publishedOn) " +
             "FROM Event e " +
             "WHERE e.id IN :ids")
-    LocalDateTime findEarliestEvent(List<Integer> ids);
+    Optional<LocalDateTime> findEarliestEvent(@Param("ids") List<Integer> ids);
 
     List<Event> findAllByInitiatorId(Integer initiatorId, PageRequest pageRequest);
 
     Optional<Event> findByIdAndInitiatorId(Integer eventId, Integer initiatorId);
 
-    List<Event> findAllByInitiatorIdInAndStateInAndCategoryIdInAndEventDateBetween(List<Integer> users,
-                                                                                   List<EventFullDto.EventState> states,
-                                                                                   List<Integer> categories,
-                                                                                   LocalDateTime rangeStart, LocalDateTime rangeEnd,
-                                                                                   PageRequest pageRequest);
+    Boolean existsEventByCategoryId(Integer catId);
 
     @Query("SELECT e FROM Event e " +
-            "WHERE (LOWER(e.annotation) LIKE LOWER(CONCAT('%', :text, '%')) OR " +
-            "       LOWER(e.description) LIKE LOWER(CONCAT('%', :text, '%'))) " +
-            "AND e.state = 'PUBLISHED' " +
+            "WHERE (:userIds IS NULL OR e.initiator.id IN :userIds) " +
+            "AND (:states IS NULL OR e.state IN :states) " +
+            "AND (:categoryIds IS NULL OR e.category.id IN :categoryIds) " +
+            "AND (COALESCE(:rangeStart, NULL) IS NULL OR e.eventDate > :rangeStart) " +
+            "AND (COALESCE(:rangeEnd, NULL) IS NULL OR e.eventDate < :rangeEnd)")
+    List<Event> findAllEventsWithFilters(
+            @Param("userIds") List<Integer> userIds,
+            @Param("states") List<EventState> states,
+            @Param("categoryIds") List<Integer> categoryIds,
+            @Param("rangeStart") LocalDateTime rangeStart,
+            @Param("rangeEnd") LocalDateTime rangeEnd,
+            Pageable pageRequest
+    );
+
+    @Query("SELECT e FROM " +
+            "Event as e " +
+            "WHERE ((:text IS NULL) OR " +
+            "(LOWER(e.annotation) LIKE %:text%) OR " +
+            "(LOWER(e.description) LIKE %:text%)) " +
             "AND (:categories IS NULL OR e.category.id IN :categories) " +
             "AND (:paid IS NULL OR e.paid = :paid) " +
-            "AND ((:rangeStart IS NULL OR :rangeEnd IS NULL) OR e.eventDate BETWEEN :rangeStart AND :rangeEnd) " +
-            "AND (:onlyAvailable = false OR (e.confirmedRequests < e.participantsLimit)) "
-//            "CASE WHEN :onlyAvailable = true THEN (e.confirmedRequests < e.participantsLimit) END " +
-//            "AND e.confirmedRequests < e.participantsLimit OR e.participantsLimit IS NULL " +
-//            "ORDER BY " +
-//            "CASE WHEN :sort = 'EVENT_DATE' THEN e.eventDate END "
-            /*"CASE WHEN :sort = 'VIEWS' THEN e.views END"*/)
-    List<Event> findPublishedEventsWithFilters(String text,
-                                               List<Integer> categories,
-                                               Boolean paid,
-                                               LocalDateTime rangeStart, LocalDateTime rangeEnd,
-                                               Boolean onlyAvailable,
-                                               PageRequest pageRequest);
+            "AND (e.eventDate IS NULL OR e.eventDate IS NOT NULL) " +
+            "AND (false = :onlyAvailable OR (true = :onlyAvailable AND e.participantLimit > 0)) " +
+            "AND (COALESCE(:start, NULL) IS NULL OR e.eventDate > :start) " +
+            "AND (COALESCE(:end, NULL) IS NULL OR e.eventDate < :end)")
+    List<Event> findPublishedEventsWithFilters(
+            @Param("text") String text,
+            @Param("categories") Collection<Integer> categories,
+            @Param("paid") Boolean paid,
+            @Param("onlyAvailable") Boolean onlyAvailable,
+            @Param("start") LocalDateTime start,
+            @Param("end") LocalDateTime end,
+            @Param("pageable") Pageable pageable
+    );
 
 }
