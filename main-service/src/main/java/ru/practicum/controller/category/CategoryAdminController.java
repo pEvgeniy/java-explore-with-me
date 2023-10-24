5b70d7dcc7d8e942eabd9e7294744e6980d5c680
pEvgeniy <popevgeniyv@yandex.ru>
2023-10-24 14:34:30 +0300

    fixed and feat category controllers

--- main-service/src/main/java/ru/practicum/controller/category/CategoryAdminController.java
+++ main-service/src/main/java/ru/practicum/controller/category/CategoryAdminController.java
@@ -1,7 +1,8 @@
-package ru.practicum.controller;
+package ru.practicum.controller.category;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -9,9 +10,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.dto.category.CategoryDto;
-import ru.practicum.service.category.CategoryService;
+import ru.practicum.service.category.CategoryAdminService;
 
 import javax.validation.Valid;
 
@@ -20,17 +22,19 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/admin/categories")
 @RequiredArgsConstructor
-public class CategoryController {
+public class CategoryAdminController {
 
-    private final CategoryService categoryService;
+    private final CategoryAdminService categoryService;
 
     @PostMapping
-    public CategoryDto createCategory(@RequestBody CategoryDto categoryDto) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public CategoryDto createCategory(@RequestBody @Valid CategoryDto categoryDto) {
         log.info("controller. post. /admin/categories. create category");
         return categoryService.createCategory(categoryDto);
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteCategory(@PathVariable Integer id) {
         log.info("controller. post. /admin/categories/{}. delete category", id);
         categoryService.deleteCategoryById(id);
